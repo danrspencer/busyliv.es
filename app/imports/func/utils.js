0840d251ed40f8c8e78b2cc7f38bd7bcc720f6fa
Dan Spencer <daniel.spencer2@skybettingandgaming.com>
2016-08-23 16:31:44 +0100

    Removed date NLP for now

--- app/imports/func/utils.js
+++ app/imports/func/utils.js
@@ -1,11 +1,12 @@
-import moment from 'moment';
+/* @flow */
 
+import moment from 'moment';
 import { curry, map, times } from 'ramda';
 
-const incrementDay = curry((day, amount) => day.clone().add(amount, 'day'));
-const generateDayList = (startDay, length) => times(incrementDay(startDay), length);
+const generateCalendar = (startDate: string, endDate: string) => {
+    const incrementDay = curry((day, amount) => day.clone().add(amount, 'day'));
+    const generateDayList = (startDay, length) => times(incrementDay(startDay), length);
 
-const generateCalendar = (startDate, endDate) => {
     const start = moment(startDate);
     const end = moment(endDate);
     const days = end.diff(start, 'days') + 1;
--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -5,41 +5,28 @@ import { generateCalendar, nlpMoment, uid } from '../func/utils.js';
 import Events from '../api/events.js';
 import Calendar from './Calendar.jsx';
 
-import date from 'date.js';
 
 export default React.createClass({
     getInitialState() {
-        const start = "today";
-        const end = "next month";
+        const start = "1990-01-01";
+        const end = "1990-01-30";
 
         return {
             start,
-            end,
-            startDate: nlpMoment(start),
-            endDate: nlpMoment(end)
+            end
         };
     },
 
     onStartDateChange(event) {
         const start = event.target.value;
-        const startDate = nlpMoment(start);
 
-        this.setState(startDate == null
-            ? { start }
-            : { start, startDate }
-        );
+        this.setState({ start });
     },
 
     onEndDateChange(event) {
         const end = event.target.value;
-        const endDate = nlpMoment(end);
 
-        date(text);
-
-        this.setState(endDate == null
-            ? { end }
-            : { end, endDate }
-        );
+        this.setState({ end });
     },
 
     createEvent() {
@@ -48,13 +35,11 @@ export default React.createClass({
             calendar: this.getCalendar()
         };
 
-        Events.insert(event);
-
-        console.log(event);
+        FlowRouter.go(event.uid);
     },
 
     getCalendar() {
-        return generateCalendar(this.state.startDate, this.state.endDate)
+        return generateCalendar(this.state.start, this.state.end)
     },
 
     render() {
@@ -68,9 +53,9 @@ export default React.createClass({
                 <br /><br />
 
                 Create event between
-                <input type="text" value={this.state.start} onChange={this.onStartDateChange} />
+                <input type="date" value={this.state.start} onChange={this.onStartDateChange} />
                 and
-                <input type="text" value={this.state.end} onChange={this.onEndDateChange} />
+                <input type="date" value={this.state.end} onChange={this.onEndDateChange} />
 
                 <br /><br /><br />
 
