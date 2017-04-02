f7075eef45b969eab2bf83736617ad10a10c225b
Dan Spencer <danrspen@gmail.com>
2017-04-02 01:56:13 +0100

    trying another version of build script

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 cd application/client
+
 echo "## Installing node dependencies ##"
 npm install
+
 echo "## Installing elm dependencies ##"
 npm run elm-package -- install -y
-cd tests
+
 echo "## Installing elm test dependencies ##"
-../node_modules/.bin/elm-package install -y
-cd ..
+(cd tests && ../node_modules/.bin/elm-package install -y)
+
 echo "## Running tests ##"
 npm test
