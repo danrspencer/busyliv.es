5a195972e69853f4a2443ffd27c4ebb8ee0f470a
Dan Spencer <danrspen@gmail.com>
2017-04-02 01:52:23 +0100

    updated build script

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+cd application/client
+echo "## Installing node dependencies ##"
+npm install
+echo "## Installing elm dependencies ##"
+npm run elm-package -- install -y
+cd tests
+echo "## Installing elm test dependencies ##"
+../node_modules/.bin/elm-package install -y
+cd ..
+echo "## Running tests ##"
+npm test
